efa17c6b460482d94e85efa7382cf63de52e200a
AdiJo2004 <143385334+AdiJo2004@users.noreply.github.com>
2025-11-14 13:04:19 +0530

    Update 22BCE1126_LAB-3.cpp

--- 22BCE1126_LAB-3.cpp
+++ 22BCE1126_LAB-3.cpp
@@ -1,17 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Keywords list
 unordered_set<string> keywords = {"int", "float", "double", "long", "return", "void", "if", "else", "while", "for"};
-
-// Operators
 unordered_set<char> singleOp = {'=', '+', '-', '*', '/', '<', '>', '!', '%'};
 unordered_set<string> multiOp = {"==","!=","<=",">=","++","--","+=","-=","*=","/="};
-
-// Special Symbols
 unordered_set<char> special = {')','(','{','}',';',','};
 
-// Symbol Table Entry
 struct SymbolEntry {
     int entryNo;
     string lexeme;
@@ -20,15 +14,12 @@ struct SymbolEntry {
     vector<int> lineUsed;
 };
 
-// Symbol Table
 vector<SymbolEntry> symbolTable;
 
-// Check keyword
 bool isKeyword(string s) {
     return keywords.find(s) != keywords.end();
 }
 
-// Check identifier rules
 bool isIdentifierStart(char c) {
     return isalpha(c) || c == '_';
 }
@@ -36,7 +27,6 @@ bool isIdentifierChar(char c) {
     return isalnum(c) || c == '_';
 }
 
-// Add to symbol table
 void addToSymbolTable(string lexeme, string type, int line) {
     // check for existing entry
     for (auto &e : symbolTable) {
@@ -45,7 +35,6 @@ void addToSymbolTable(string lexeme, string type, int line) {
             return;
         }
     }
-    // add new entry
     SymbolEntry newEntry;
     newEntry.entryNo = symbolTable.size() + 1;
     newEntry.lexeme = lexeme;
@@ -55,7 +44,6 @@ void addToSymbolTable(string lexeme, string type, int line) {
     symbolTable.push_back(newEntry);
 }
 
-// main processing function
 void process(const string &filename) {
     ifstream file(filename);
     if (!file.is_open()) {
@@ -74,20 +62,16 @@ void process(const string &filename) {
         while (i < len) {
             char c = line[i];
 
-            // Skip whitespaces
             if (isspace(c)) { i++; continue; }
 
-            // Single line comment
             if (c == '/' && i+1 < len && line[i+1] == '/') break;
 
-            // Start of multi-line comment
             if (c == '/' && i+1 < len && line[i+1] == '*') {
                 inMultiComment = true;
                 i += 2;
                 continue;
             }
 
-            // End of multi-line comment
             if (inMultiComment) {
                 if (c == '*' && i+1 < len && line[i+1] == '/') {
                     inMultiComment = false;
@@ -96,7 +80,6 @@ void process(const string &filename) {
                 continue;
             }
 
-            // ? STRING LITERAL
             if (c == '"') {
                 string literal = "";
                 i++; 
@@ -113,7 +96,6 @@ void process(const string &filename) {
                 continue;
             }
 
-            // ? IDENTIFIER or KEYWORD
             if (isIdentifierStart(c)) {
                 string token = "";
                 while (i < len && isIdentifierChar(line[i])) {
@@ -128,7 +110,6 @@ void process(const string &filename) {
                 continue;
             }
 
-            // ? NUMBERS
             if (isdigit(c)) {
                 string number = "";
                 bool isFloat = false;
@@ -150,7 +131,6 @@ void process(const string &filename) {
                 continue;
             }
 
-            // ? MULTICHAR OPERATOR
             if (i+1 < len) {
                 string op = string(1, c) + line[i+1];
                 if (multiOp.find(op) != multiOp.end()) {
@@ -160,29 +140,26 @@ void process(const string &filename) {
                 }
             }
 
-            // ? SINGLE OPERATOR
+        
             if (singleOp.find(c) != singleOp.end()) {
                 cout << "Operator: " << c << endl;
                 i++;
                 continue;
             }
 
-            // ? SPECIAL SYMBOL
             if (special.find(c) != special.end()) {
                 cout << "Special Symbol: " << c << endl;
                 i++;
                 continue;
             }
 
-            // ? INVALID TOKEN
             cout << "Lexical Error: Unrecognized symbol '" << c << "' at line " << lineNo << endl;
             i++;
         }
     }
 
     file.close();
-
-    // ? PRINT SYMBOL TABLE
+    
     cout << "\n===== SYMBOL TABLE =====\n";
     cout << "Entry\tLexeme\t\tToken Type\tDeclared\tUsed Lines\n";
     for (auto &e : symbolTable) {
@@ -198,6 +175,7 @@ int main() {
     cin >> name;
     process(name);
 }
+
 
 
 
